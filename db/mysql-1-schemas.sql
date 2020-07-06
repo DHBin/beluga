cf62e58aa2beb20a7f1a7566c81f7183d6d76bce
DHB <xx158@qq.com>
2020-07-06 15:26:36 +0800

    docs: 添加sql文件

---
+++ db/mysql-1-schemas.sql
@@ -0,0 +1,123 @@
+-- 菜单表
+CREATE TABLE `sys_menu`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `label`       varchar(20)         NOT NULL COMMENT '名称',
+    `description` varchar(100)        NULL COMMENT '说明',
+    `type`        tinyint(2)          NOT NULL COMMENT '1-顶部菜单 2-左侧菜单',
+    `path`        varchar(500)        NULL COMMENT '前端页面路径',
+    `component`   varchar(500)        NULL COMMENT '前端组件路径',
+    `icon`        varchar(500)        NULL COMMENT '菜单图标',
+    `num`         int(11)             NOT NULL COMMENT '编号',
+    `parent_num`  int(11)             NOT NULL COMMENT '父级编码',
+    `order_num`   int(11)             NOT NULL COMMENT '顺序',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '菜单';
+
+
+-- 权限表
+CREATE TABLE `sys_perm`
+(
+    `id`             varchar(100)        NOT NULL COMMENT 'id',
+    `own`            varchar(100)        NULL COMMENT '所属项目',
+    `name`           varchar(100)        NULL COMMENT '名称',
+    `description`    varchar(100)        NULL COMMENT '说明',
+    `path`           varchar(256)        NULL COMMENT '路径',
+    `method`         varchar(256)        NULL COMMENT '请求方法',
+    `authorizations` varchar(256)        NULL COMMENT '权限标识',
+    `create_time`    datetime            NOT NULL COMMENT '创建时间',
+    `update_time`    datetime            NULL COMMENT '更新时间',
+    `create_uid`     bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`     bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '权限';
+
+-- 菜单-权限关系表
+CREATE TABLE `sys_menu_perm`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `mid`         bigint(20) UNSIGNED NOT NULL COMMENT '菜单id',
+    `pid`         varchar(100)        NOT NULL COMMENT '权限id',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '菜单-权限关系';
+
+-- 角色表
+CREATE TABLE `sys_role`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `name`        varchar(20)         NOT NULL COMMENT '角色名',
+    `role_key`    varchar(20)         NOT NULL COMMENT '角色标识',
+    `description` varchar(255)        NULL COMMENT '说明',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '角色';
+
+
+-- 角色-菜单关系表
+CREATE TABLE `sys_role_menu`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `rid`         bigint(20) UNSIGNED NOT NULL COMMENT '用户id',
+    `mid`         bigint(20) UNSIGNED NOT NULL COMMENT '菜单id',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '角色-菜单关系';
+
+-- 角色-权限关系表
+CREATE TABLE `sys_role_perm`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `rid`         bigint(20) UNSIGNED NOT NULL COMMENT '角色id',
+    `pid`         varchar(100)        NOT NULL COMMENT '权限id',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '角色-权限';
+
+
+-- 用户表
+CREATE TABLE `sys_user`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `username`    varchar(20)         NOT NULL COMMENT '用户名',
+    `phone`       varchar(20)         NOT NULL COMMENT '手机号码',
+    `email`       varchar(100)        NOT NULL COMMENT '邮箱',
+    `password`    varchar(255)        NOT NULL COMMENT '密码',
+    `dept_id`     bigint(20) UNSIGNED NULL COMMENT '部门id',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    `deleted`     tinyint(1)          NOT NULL DEFAULT 0 COMMENT '1为已删除，0为未删除',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `sys_user_username_uindex` (`username`) USING BTREE
+) COMMENT '用户';
+
+-- 用户-角色关系表
+CREATE TABLE `sys_user_role`
+(
+    `id`          bigint(20) UNSIGNED NOT NULL COMMENT 'id',
+    `user_id`         bigint(20) UNSIGNED NOT NULL COMMENT '用户id',
+    `rid`         bigint(20) UNSIGNED NOT NULL COMMENT '角色id',
+    `create_time` datetime            NOT NULL COMMENT '创建时间',
+    `update_time` datetime            NULL COMMENT '更新时间',
+    `create_uid`  bigint(20) UNSIGNED NOT NULL COMMENT '创建uid',
+    `update_uid`  bigint(20) UNSIGNED NULL COMMENT '更新uid',
+    PRIMARY KEY (`id`) USING BTREE
+) COMMENT '用户-角色关系';
---
+++ db/oracle-1-schemas.sql
@@ -0,0 +1,123 @@
+-- 菜单表
+CREATE TABLE sys_menu
+(
+    id          number(20)   NOT NULL,
+    label       varchar(100) NOT NULL,
+    description varchar(100) NULL,
+    type        number(2)    NOT NULL,
+    path        varchar(500) NULL,
+    component   varchar(500) NULL,
+    icon        varchar(500) NULL,
+    num         number(11)   NOT NULL,
+    parent_num  number(11)   NOT NULL,
+    order_num   number(11)   NOT NULL,
+    create_time date         NOT NULL,
+    update_time date         NULL,
+    create_uid  number(20)   NOT NULL,
+    update_uid  number(20)   NULL,
+    PRIMARY KEY (id)
+);
+
+
+-- 权限表
+CREATE TABLE sys_perm
+(
+    id             varchar(100) NOT NULL,
+    own            varchar(100) NULL,
+    name           varchar(100) NULL,
+    description    varchar(100) NULL,
+    path           varchar(256) NULL,
+    method         varchar(256) NULL,
+    authorizations varchar(256) NULL,
+    create_time    date         NOT NULL,
+    update_time    date         NULL,
+    create_uid     number(20)   NOT NULL,
+    update_uid     number(20)   NULL,
+    PRIMARY KEY (id)
+);
+
+-- 菜单-权限关系表
+CREATE TABLE sys_menu_perm
+(
+    id          number(20)   NOT NULL,
+    mid         number(20)   NOT NULL,
+    pid         varchar(100) NOT NULL,
+    create_time date         NOT NULL,
+    update_time date         NULL,
+    create_uid  number(20)   NOT NULL,
+    update_uid  number(20)   NULL,
+    PRIMARY KEY (id)
+);
+
+-- 角色表
+CREATE TABLE sys_role
+(
+    id          number(20)   NOT NULL,
+    name        varchar(20)  NOT NULL,
+    role_key    varchar(20)  NOT NULL,
+    description varchar(255) NULL,
+    create_time date         NOT NULL,
+    update_time date         NULL,
+    create_uid  number(20)   NOT NULL,
+    update_uid  number(20)   NULL,
+    PRIMARY KEY (id)
+);
+
+
+-- 角色-菜单关系表
+CREATE TABLE sys_role_menu
+(
+    id          number(20) NOT NULL,
+    rid         number(20) NOT NULL,
+    mid         number(20) NOT NULL,
+    create_time date       NOT NULL,
+    update_time date       NULL,
+    create_uid  number(20) NOT NULL,
+    update_uid  number(20) NULL,
+    PRIMARY KEY (id)
+);
+
+-- 角色-权限关系表
+CREATE TABLE sys_role_perm
+(
+    id          number(20)   NOT NULL,
+    rid         number(20)   NOT NULL,
+    pid         varchar(100) NOT NULL,
+    create_time date         NOT NULL,
+    update_time date         NULL,
+    create_uid  number(20)   NOT NULL,
+    update_uid  number(20)   NULL,
+    PRIMARY KEY (id)
+);
+
+-- 用户表
+CREATE TABLE sys_user
+(
+    id          number(20)   NOT NULL,
+    username    varchar(20)  NOT NULL,
+    phone       varchar(20)  NOT NULL,
+    email       varchar(100) NOT NULL,
+    password    varchar(255) NOT NULL,
+    dept_id     number(20)   NULL,
+    create_time date         NOT NULL,
+    update_time date         NULL,
+    create_uid  number(20)   NOT NULL,
+    update_uid  number(20)   NULL,
+    deleted     number(1)    NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create UNIQUE INDEX sys_user_username_uindex on sys_user (username);
+
+-- 用户-角色关系表
+CREATE TABLE sys_user_role
+(
+    id          number(20) NOT NULL,
+    user_id     number(20) NOT NULL,
+    rid         number(20) NOT NULL,
+    create_time date       NOT NULL,
+    update_time date       NULL,
+    create_uid  number(20) NOT NULL,
+    update_uid  number(20) NULL,
+    PRIMARY KEY (id)
+);
